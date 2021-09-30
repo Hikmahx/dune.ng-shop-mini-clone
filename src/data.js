58bde0fc4639989a3b73c11851b212d5e64c10a4
Hikmahx <hikmayousuph@gmail.com>
2021-09-30 03:29:54 +0100

    sliderItems array added to data.js

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const sliderItems = [
+  {
+    id:1, 
+    img: 'assets/5G-Milk.png',
+    alt:'5G-Milk',
+    title:'THE TASTE OF PERFECTION IN A BAR', 
+    desc: 'Made from high-quality ingredients and all-natural cocoa, Dùne Chocolate comes in mouth-watering variants that bring perfection to your taste buds',
+    more: 'A PRODUCT TO MATCH YOUR TASTE →', 
+    bg:'#ffffff'
+  },
+  {
+    id:2, 
+    img: 'assets/hq-chocolates.png',
+    alt: 'hq-chocolates',
+    title: 'LOCAL EMPOWERMENT, PREMIUM QUALITY, FRESH DELIVERY',
+    desc:'We are committed to making delightful chocolates that serve a local market while changing the lives of those who make it possible.',
+    more:'MORE ABOUT OUR COMMITMENTS →',
+    bg:'#fefbf6'
+  },
+  {
+    id:3, 
+    img: 'assets/chocolate-bar.png',
+    alt:'chocolate-bar',
+    title: 'TRANSFORMING LIVES, ONE BITE AT A TIME',
+    desc:'Through our unique operational model, each bar of chocolate bought is directly contributing to the grassroot economy and empowering the chain of local hands.',
+    more:'READ MORE →',
+    bg:'#f9f9f9'
+  }
+]
