ae2bf33b091b41aa61d7cff33bcfef660f25e1d6
Hikmahx <hikmayousuph@gmail.com>
2021-10-01 03:14:34 +0100

    categories array in data.js map thru and into item

---
+++ src/components/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { categories } from '../data'
+import CategoryItem from './CategoryItem'
+
+const Categories = () => {
+  return (
+    <div className="m-8 h-screen flex flex-col lg:flex-row justify-between items-center gap-4 py-2">
+            {categories.map((item)=>(
+        <CategoryItem item={item} />
+      ))}
+    </div>
+  )
+}
+
+export default Categories
